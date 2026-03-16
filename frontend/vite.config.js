3cf353cd3c31767702deb5e4175eee6d6020f6f2
x <x@gmail.com>
2026-03-16 22:52:20 +0800

    x

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,17 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 export default defineConfig({
   plugins: [svelte()],
   server: {
-    port: 5173,
+    host: '0.0.0.0',
+    port: 8080,
+    strictPort: true,
     proxy: {
       '/api': 'http://130.94.33.164:8081',
       '/downloads': 'http://130.94.33.164:8081'
     }
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 8080,
+    strictPort: true
   }
 });
